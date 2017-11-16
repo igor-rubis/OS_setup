785103f366232a5bd48637e3b653bf1f993de1e3
Igor Rubis <rubis.igor@gmail.com>
2017-11-16 12:54:13 +0200

    Update install_gradle.sh

--- ubuntu/install_gradle.sh
+++ ubuntu/install_gradle.sh
@@ -2,3 +2,11 @@
 
 mkdir /opt/gradle
 unzip -d /opt/gradle gradle-4.3.1-bin.zip
+
+echo "" >> /etc/profile
+echo "GRADLE_HOME=/opt/gradle/gradle-4.3.1" >> /etc/profile
+echo "export PATH=\$PATH:\$GRADLE_HOME/bin" >> /etc/profile
+
+. /etc/profile
+
+gradle -version
