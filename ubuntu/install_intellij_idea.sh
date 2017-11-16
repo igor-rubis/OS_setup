451c97f928bcc05585942118679e1ab24f68fc32
Igor Rubis <rubis.igor@gmail.com>
2017-11-16 16:44:24 +0200

    Update install_intellij_idea.sh

--- ubuntu/install_intellij_idea.sh
+++ ubuntu/install_intellij_idea.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+ # Copyright 2016 Igor Rubis
+ # Licensed under the Apache License, Version 2.0
+
+# chmod +x /path/to/install_docker.sh
+# /path/to/install_docker.sh || ./install_docker.sh
+
 ARCHIVE="ideaIC-2017.2.6.tar.gz"
 FOLDER="idea-IC-172.4574.11"
 
