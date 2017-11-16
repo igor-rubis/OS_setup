b978fde107be900a82c986e2d43d752922a321f4
Igor Rubis <rubis.igor@gmail.com>
2017-11-16 16:52:50 +0200

    Update install_docker.sh

--- ubuntu/install_docker.sh
+++ ubuntu/install_docker.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
- # Copyright 2016 Igor Rubis
- # Licensed under the Apache License, Version 2.0
-
-# chmod +x /path/to/install_docker.sh
-# /path/to/install_docker.sh || ./install_docker.sh
-
 # Uninstall old versions
 apt-get remove docker docker-engine docker.io
 
