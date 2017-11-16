455e8979fd067c704e2596312994076dfa53cbff
Igor Rubis <rubis.igor@gmail.com>
2017-11-16 16:53:12 +0200

    Update install_git.sh

--- ubuntu/install_git.sh
+++ ubuntu/install_git.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
- # Copyright 2016 Igor Rubis
- # Licensed under the Apache License, Version 2.0
-
-# chmod +x /path/to/install_docker.sh
-# /path/to/install_docker.sh || ./install_docker.sh
-
 apt-get install git
